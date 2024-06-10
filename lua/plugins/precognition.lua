4928cf5f75ac644c785f2e1835a0bdfbafb48faf
Osamu Aoki <osamu@debian.org>
2024-06-10 16:17:02 +0900

    precognition.lua: add extra package
    Set to start as invisible
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

---
+++ lua/plugins/precognition.lua
@@ -0,0 +1,12 @@
+-- uncomment to do nothing here and return an empty spec
+-- stylua: ignore
+--if true then return {} end
+
+-- Extra plugin package
+-- https://github.com/tris203/precognition.nvim
+return {
+    "tris203/precognition.nvim",
+    config = {
+      startVisible = false,
+    },
+}
