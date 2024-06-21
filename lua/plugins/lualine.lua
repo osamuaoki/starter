bfb07c8b3fe4594b96ae836b66a7c3bdb8882bed
Osamu Aoki <osamu@debian.org>
2024-06-21 10:47:45 +0900

    Match plugin config file names
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -22,13 +22,4 @@ return {
       }
     end,
   },
-  -- Initializable notify delay --- read it via `<leader>snh` if you miss
-  -- Cool menu animation slide out
-  {
-    "rcarriga/nvim-notify",
-    opts = function(_, opts)
-      opts.timeout = vim.g.notify_timeout and vim.g.notify_timeout or 3000
-      opts.stages = "fade_in_slide_out"
-    end,
-  },
 }
