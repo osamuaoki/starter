c78bffa296a5d9558f6378346f06656eb3aff5f8
Osamu Aoki <osamu@debian.org>
2024-06-22 14:31:30 +0900

    nvim-lspconfig.lua
    Signed-off-by: Osamu Aoki <osamu@debian.org>

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,27 @@
+-- initial set for linlay_hints via vim.g.inlay_hint_disable
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = function(_, opts)
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        opts.inlay_hints.enabled = not vim.g.inlay_hint_disable
+        opts.servers = {
+          lua_ls = {
+            -- mason = false, -- set to false if you don't want this server to be installed with mason
+            -- Use this to add any additional keymaps
+            -- for specific lsp servers
+            -- ---@type LazyKeysSpec[]
+            -- keys = {},
+            settings = {
+              Lua = {
+                diagnostics  = {
+                  globals = {"vim", "LazyVim"},
+                },
+              },
+            },
+          },
+        }
+    end,
+  },
+}
