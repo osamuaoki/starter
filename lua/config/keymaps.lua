6f6be62f5b11da5d945bf60f2a583b3a63f99b71
Osamu Aoki <osamu@debian.org>
2025-08-27 22:46:49 +0900

    keymaps.lua: simple remappings
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,22 @@ local map = vim.keymap.set
 map("i", "jk", "<esc>", { desc = "Exit insert-mode", remap = true })
 map("t", "jk", "<C-\\><C-n>", { desc = "Exit term-mode", remap = true })
 map("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit term-mode", remap = true })
+
+-- update which-key description with function (no icon change)
+local wk_update_desc = function(lhs, desc_fn)
+  local ok, wk = pcall(require, "which-key")
+  if ok then
+    wk.add({
+      {
+        lhs,
+        desc = desc_fn,
+      },
+    })
+  end
+end
+
+-- Disable accidental execution of 'u' in normal mode after '<leader>u'
+map("n", "<leader>uu", function() end, { desc = "which_key_ignore" }) -- hidden
+
+-- LazyExtras '<leader>uX' make these accessible without dashboard screen
+map("n", "<leader>uX", "<cmd> LazyExtras <cr>", { desc = "Lazy Extras †" })
