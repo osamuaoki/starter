f22cf48d3ebcf405bad353e243346d0b67417db0
Osamu Aoki <osamu@debian.org>
2025-08-27 22:35:17 +0900

    autocmds.lua: tailspace
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,37 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- ===========================================================================
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_user_" .. name, { clear = true })
+end
+
+-- Highlight with color RED for tailspace excluding ones before cursor
+vim.cmd.highlight([[Tailspace ctermbg=red guibg=red]])
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("tailing_whitespace"),
+  callback = function(event)
+    local exclude = {
+      "snacks_notif",
+      "blink-cmp-menu",
+      "blink-cmp-documentation",
+      "lazy",
+      "mason",
+      --"fzf",
+      --"noice",
+    }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or not vim.g.tailspace then
+      -- //            Match nothing
+      vim.cmd.match([[Tailspace //]])
+    else
+      -- /\s\+/        1 or more whitespace character: <Space> and <Tab>
+      -- /\%#\@<!/     Matches with zero width if the cursor position does NOT match.
+      vim.cmd.match([[Tailspace /\s\+\%#\@<!$/]])
+      if vim.g.tailspace_debug and vim.bo[buf].filetype ~= "snacks_notif" then
+        vim.notify(("Filetype = %s"):format(vim.bo[buf].filetype))
+      end
+    end
+  end,
+})
