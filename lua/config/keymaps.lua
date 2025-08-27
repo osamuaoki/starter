ad4ca2ff0b444cef56f1016317fbc92bb3ca68af
Osamu Aoki <osamu@debian.org>
2025-08-27 22:44:11 +0900

    keymaps.lua: quick jk for <esc>
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- DO NOT USE `LazyVim.safe_keymap_set` IN HERE!!
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+
+-- Easy ESC-key.  (simpler than "max397574/better-escape.nvim")
+-- Ensure vim.opt.timeoutlen to be 200 (QMK-like) - 300 (LazyVim-default) to be responsive
+map("i", "jk", "<esc>", { desc = "Exit insert-mode", remap = true })
+map("t", "jk", "<C-\\><C-n>", { desc = "Exit term-mode", remap = true })
+map("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit term-mode", remap = true })
